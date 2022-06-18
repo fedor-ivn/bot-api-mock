bb94ce4a3644a720dfad4d535ecb6be3d7845108
Fedor Ivanov <ivnfedor@gmail.com>
2022-06-18 20:18:54 +0300

    Refactor `Message`

--- src/State/Message.hs
+++ src/State/Message.hs
@@ -1,14 +1,12 @@
-module State.Message (Message (Message)) where
+module State.Message (Message (..)) where
 
-import Data.Dates
-import Data.Text
-import State.Id
-
---import Data.Time.Calendar
+import Data.Dates ( DateTime )
+import Data.Text ( Text )
+import State.Id ( Id )
 
 data Message = Message
   { id :: Id,
     from :: Id,
     text :: Text,
-    date :: DateTime -- need to be confirmed. Is it the right format?
+    date :: DateTime
   }
