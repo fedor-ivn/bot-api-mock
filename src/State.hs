92ee666dc0cd7852fc54dc7af126f98d6170b52e
Fedor Ivanov <ivnfedor@gmail.com>
2022-06-18 20:20:34 +0300

    Implement methods for the state
    Co-Authored-By: SnejUgal <contact@snejugal.ru>

--- src/State.hs
+++ src/State.hs
@@ -1,33 +1,90 @@
-module State (State (State)) where
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
+module State (State (..), getBot, sendMessage) where
+
+import qualified Control.Monad.State as StateM
+import Data.Bool (Bool (False))
+import Data.Dates (DateTime)
+import Data.List (find)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Sequence
+import Data.Maybe (Maybe (Nothing), fromMaybe, listToMaybe)
+import Data.Sequence (Seq)
+import Data.Text (Text)
 import Server.Token (Token)
-import State.Chat
-import State.Id
-import State.Message (Message)
+import qualified Server.Token as Token
+import State.Chat (PrivateChat (PrivateChat), makeId)
+import qualified State.Chat as Chat
+import State.Id (Id (Id))
+import State.Message (Message (Message))
+import qualified State.Message as Message
 import State.Update (Update)
-import State.User
+import State.User (User (User))
+import qualified State.User as User
+
+-- The IDs are sorted in ascending order, as otherwise (1, 2) and (2, 1)
+-- would map to different chats. Use `PrivateChat.makeChatId` to generate such
+-- an id.
+type PrivateChats = Map (Id, Id) PrivateChat
 
 data State = State
   { users :: [User],
-    -- The IDs are sorted in ascending order, as otherwise (1, 2) and (2, 1)
-    -- would map to different chats.
-    privateChats :: Map (Id, Id) PrivateChat,
-    bots :: Map Id Bot
+    privateChats :: PrivateChats,
+    bots :: Map Token Bot
   }
 
-data Bot = Bot
-  { token :: Token,
-    updates :: Sequence Update
+newtype Bot = Bot
+  { updates :: Seq Update
   }
 
--- User
--- [Chat] -> Chat Map<(user1Id,user2Id) : [Message]>
--- Map <botid : {token::Token, updatesTohadle :: Queue Update}>
+-- | Return a list of users from the State.
+getUsers :: StateM.State State [User]
+getUsers = do
+  State {users} <- StateM.get
+  return users
 
--- data Update = { id :: integer, message :: Message (to be cont.)}
+-- | Return a user by Id from the State.
+getUser :: Id -> StateM.State State (Maybe User)
+getUser id = do
+  users <- getUsers
+  let user = Data.List.find (\x -> User.id x == id) users
+  return user
 
--- getUser :: State -> Id -> Maybe User
--- sendMessage :: State -> Id -> Id -> Text -> State
+-- | Return a list of private chats from the State.
+getPrivateChats :: StateM.State State PrivateChats
+getPrivateChats = do
+  State {privateChats} <- StateM.get
+  return privateChats
+
+-- | Return private chat by Id from the State.
+getPrivateChat :: (Id, Id) -> StateM.State State (Maybe PrivateChat)
+getPrivateChat chatId = do
+  privateChats <- getPrivateChats
+  return (Map.lookup chatId privateChats)
+
+-- | Add new private chat to the State.
+putPrivateChat :: (Id, Id) -> PrivateChat -> StateM.State State ()
+putPrivateChat chatId chat = do
+  state@State {privateChats} <- StateM.get
+  StateM.put (state {privateChats = Map.insert chatId chat privateChats})
+
+-- | Return a bot by its token from the State.
+getBot :: Token -> StateM.State State (Maybe User)
+getBot token = do
+  users <- getUsers
+  case Token.getId token of
+    Nothing -> return Nothing
+    Just id -> getUser id
+
+-- | Send new message in private chat.
+sendMessage :: Id -> Id -> DateTime -> Text -> StateM.State State ()
+sendMessage from to date text = do
+  let chatId = Chat.makeId from to
+
+  chat <- getPrivateChat chatId
+  let chat' = fromMaybe Chat.empty chat
+  let updatedChat = Chat.addMessage chat' from date text
+
+  putPrivateChat chatId updatedChat
