3eb586e519c8ef6f03778c38b4fd68521637e6d1
Fedor Ivanov <ivnfedor@gmail.com>
2022-06-19 22:55:13 +0300

    Implement `BotPermissions`

---
+++ src/ServerState/BotPermissions.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module ServerState.BotPermissions
+  ( BotPermissions (..),
+    defaultPermissions,
+  )
+where
+
+import Data.Aeson
+  ( Options (fieldLabelModifier),
+    ToJSON (toJSON),
+    camelTo2,
+    defaultOptions,
+    genericToJSON,
+  )
+import GHC.Generics (Generic)
+
+-- | A bot's permissions.
+data BotPermissions = BotPermissions
+  { canJoinGroups :: Bool,
+    canReadAllGroupMessages :: Bool,
+    supportsInlineQueries :: Bool
+  }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON BotPermissions where
+  toJSON = genericToJSON options
+    where
+      options = defaultOptions {fieldLabelModifier = camelTo2 '_'}
+
+-- | All bot permissions are set to `False`.
+defaultPermissions :: BotPermissions
+defaultPermissions =
+  BotPermissions
+    { canJoinGroups = False,
+      canReadAllGroupMessages = False,
+      supportsInlineQueries = False
+    }
