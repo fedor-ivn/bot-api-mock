6d48701223637f026eaa37f888c4abc7abff3dd2
Fedor Ivanov <ivnfedor@gmail.com>
2022-06-22 22:29:24 +0300

    Remove unused imports in `ServerState.Message`

--- src/ServerState/Message.hs
+++ src/ServerState/Message.hs
@@ -1,18 +1,12 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module ServerState.Message (Message (..)) where
 
-import Control.Monad.Cont (MonadIO (liftIO))
-import Data.Aeson (ToJSON, object, (.=))
-import Data.Aeson.Types (ToJSON (toJSON))
+import Data.Aeson (ToJSON)
 import Data.Text (Text)
-import Data.Time.Clock (UTCTime (utctDay), getCurrentTime, utctDayTime)
 import GHC.Generics (Generic)
 import ServerState.Id (Id)
 import ServerState.Time (Time)
-import Prelude hiding (id)
 
 data Message = Message
   { id :: Id,
