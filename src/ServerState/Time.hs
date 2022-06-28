cc41a1f2baade1abfa58931d09627e7a60acf9aa
Fedor Ivanov <ivnfedor@gmail.com>
2022-06-28 15:14:11 +0300

    fix time

--- src/ServerState/Time.hs
+++ src/ServerState/Time.hs
@@ -1,9 +1,16 @@
 module ServerState.Time where
 
 import Data.Aeson (ToJSON (toJSON))
-import Data.Time.Clock (UTCTime (utctDayTime))
+import Data.Time.Clock (UTCTime (utctDayTime), nominalDiffTimeToSeconds)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
 
 newtype Time = Time UTCTime
 
 instance ToJSON Time where
-  toJSON (Time time) = toJSON (floor $ utctDayTime time :: Integer)
+  toJSON (Time time) = toJSON (sinceEpoch time)
+    where
+      sinceEpoch :: UTCTime -> Integer -- Convert to Unix time
+      sinceEpoch =
+        floor
+          . nominalDiffTimeToSeconds
+          . utcTimeToPOSIXSeconds
