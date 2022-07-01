26ea4709041b502d0b2a82c7b1728deba87f4ba4
Fedor Ivanov <ivnfedor@gmail.com>
2022-07-01 14:11:40 +0300

    Format the example test

--- example/tests/Test.hs
+++ example/tests/Test.hs
@@ -2,23 +2,25 @@
 
 module Main where
 
-import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.List.NonEmpty (NonEmpty((:|)))
 import Data.Maybe (fromJust)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Network.Wai.Handler.Warp (Settings, defaultSettings, setPort)
+import System.Process (CreateProcess(env), createProcess, proc)
+
 import Server (startServer, waitForAction)
-import Server.Actions (ActionKind (GetMe))
+import Server.Actions (ActionKind(SendMessage))
 import qualified Server.Token as Token
+
 import ServerState (ServerState)
 import qualified ServerState
 import qualified ServerState.BotPermissions as BotPermissions
-import ServerState.Id (Id (Id))
-import ServerState.InitialBot (InitialBot (InitialBot))
+import ServerState.Id (Id(Id))
+import ServerState.InitialBot (InitialBot(InitialBot))
 import qualified ServerState.InitialBot as InitialBot
-import ServerState.User (User (User))
+import ServerState.User (User(User))
 import qualified ServerState.User as User
-import System.Process (CreateProcess (env), createProcess, proc)
 
 botToken :: Text
 botToken = "2:random-characters"
@@ -29,22 +31,20 @@ botId = Id 2
 initialState :: ServerState
 initialState = ServerState.initialize (user :| []) (bot :| [])
   where
-    user =
-      User
-        { User.userId = Id 1,
-          User.firstName = "User",
-          User.lastName = Nothing,
-          User.username = Nothing,
-          User.isBot = False
+    user = User
+        { User.userId = Id 1
+        , User.firstName = "User"
+        , User.lastName = Nothing
+        , User.username = Nothing
+        , User.isBot = False
         }
-    bot =
-      InitialBot
+    bot = InitialBot
         { -- SAFETY: `Token.parse` is called on a constant, so we can guarantee
           -- that it returns `Just`. Calling `fromJust` on a `Just` is safe.
-          InitialBot.token = fromJust (Token.parse botToken),
-          InitialBot.name = "Bot",
-          InitialBot.username = "a_bot",
-          InitialBot.permissions = BotPermissions.defaultPermissions
+          InitialBot.token = fromJust (Token.parse botToken)
+        , InitialBot.name = "Bot"
+        , InitialBot.username = "a_bot"
+        , InitialBot.permissions = BotPermissions.defaultPermissions
         }
 
 serverSettings :: Settings
@@ -55,18 +55,18 @@ botPath = "./bot/target/release/bot"
 
 botEnv :: [(String, String)]
 botEnv =
-  [ ("BOT_TOKEN", Text.unpack botToken),
-    ("BOT_API_URI", "http://localhost:8081/")
-  ]
+    [ ("BOT_TOKEN", Text.unpack botToken)
+    , ("BOT_API_URI", "http://localhost:8081/")
+    ]
 
 startBot :: IO ()
 startBot = do
-  _ <- createProcess ((proc botPath []) {env = Just botEnv})
-  return ()
+    _ <- createProcess ((proc botPath []) { env = Just botEnv })
+    return ()
 
 main :: IO ()
 main = startServer initialState serverSettings $ \server -> do
-  startBot
-  waitForAction botId GetMe server
-  putStrLn "The bot has called `getMe`. Yay!"
-  return ()
+    startBot
+    waitForAction botId SendMessage server
+    putStrLn "The bot has called `getMe`. Yay!"
+    return ()
