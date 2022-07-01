695de7eae890096594d5e5adc01f6c04a1da4efd
Fedor Ivanov <ivnfedor@gmail.com>
2022-07-01 17:02:32 +0300

    fix Updates description

--- src/ServerState/Bot.hs
+++ src/ServerState/Bot.hs
@@ -1,32 +1,31 @@
 {-# OPTIONS_GHC -Wno-missing-fields #-}
 
-module ServerState.Bot (Bot(..), addUpdate) where
+module ServerState.Bot (Bot (..), addUpdate) where
 
 import Data.Sequence (Seq, (|>))
-
 import Server.Token (Token)
-
 import ServerState.BotPermissions (BotPermissions)
 import ServerState.CompleteMessage (CompleteMessage)
 import ServerState.Id (Id)
-import ServerState.Update (Update(Update))
+import ServerState.Update (Update (Update))
 
--- | We push elements to the left end
--- | and get element from the right end
+-- | We push elements to the right end
+-- | and get element from the left end
 type Updates = Seq Update
 
 -- | Additional information about a bot.
 data Bot = Bot
-    { token :: Token
-    , permissions :: BotPermissions
-    , updates :: Updates
-    , updateId :: Id
-    }
+  { token :: Token,
+    permissions :: BotPermissions,
+    updates :: Updates,
+    updateId :: Id
+  }
 
 addUpdate :: Maybe Bot -> CompleteMessage -> Maybe Bot
 addUpdate Nothing _ = Nothing
-addUpdate (Just bot) message = Just
-    $ bot { updates = upds |> newUpdate, updateId = nextUpdateId }
+addUpdate (Just bot) message =
+  Just $
+    bot {updates = upds |> newUpdate, updateId = nextUpdateId}
   where
     upds = updates bot
     nextUpdateId = succ (updateId bot)
