1a6dd8d0dbc5bb14e41b3645e4707a8b20850e53
Fedor Ivanov <ivnfedor@gmail.com>
2022-07-01 22:12:08 +0300

    remove warnings

--- src/Api/GetUpdates.hs
+++ src/Api/GetUpdates.hs
@@ -7,7 +7,6 @@ import Control.Monad.State (MonadIO(liftIO), State, evalState)
 import Data.Aeson (FromJSON)
 import qualified Data.Foldable as Seq
 import Data.Maybe (fromMaybe)
-import Data.Sequence (ViewR((:>), EmptyR), viewr)
 import GHC.Conc (atomically, readTVar)
 import GHC.Generics (Generic)
 import Servant (Handler)
@@ -24,32 +23,29 @@ import qualified ServerState.Update as Update
 import qualified ServerState.User as User
 
 data GetUpdates = GetUpdates
-    { offset :: Maybe Id
-    , limit :: Maybe Int
-    }
-    deriving Generic
+  { offset :: Maybe Id
+  , limit :: Maybe Int
+  }
+  deriving Generic
 
 instance FromJSON GetUpdates
 
-isOldUpdate :: Id -> Update -> Bool
-isOldUpdate id Update { Update.updateId } = updateId < id
-
 getRange :: Id -> Int -> [Update] -> [Update]
-getRange firstId limit updates =
-    take limit (dropWhile (isOldUpdate firstId) updates)
+getRange firstId limit updates = take limit (dropWhile isOldUpdate updates)
+  where isOldUpdate Update { Update.updateId } = updateId < firstId
 
 getUpdates' :: GetUpdates -> Bot -> State ServerState (Response [Update])
 getUpdates' GetUpdates { offset, limit } Bot { Bot.updates } = return
-    (Ok (getRange offset' limit' updates'))
-  where
-    updates' = Seq.toList updates
-    limit' = fromMaybe 100 limit
-    offset' = fromMaybe (Id 0) offset
+  (Ok (getRange offset' limit' updates'))
+ where
+  updates' = Seq.toList updates
+  limit' = fromMaybe 100 limit
+  offset' = fromMaybe (Id 0) offset
 
 getUpdates :: Context -> GetUpdates -> Handler (Response [Update])
 getUpdates Context { state, bot, botUser, actions } parameters = do
-    writeAction (User.userId botUser) actions Actions.GetUpdates
-    liftIO $ atomically $ do
-        state' <- readTVar state
-        let response = evalState (getUpdates' parameters bot) state'
-        return response
+  writeAction (User.userId botUser) actions Actions.GetUpdates
+  liftIO $ atomically $ do
+    state' <- readTVar state
+    let response = evalState (getUpdates' parameters bot) state'
+    return response
