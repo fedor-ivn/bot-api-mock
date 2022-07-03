d689007e9460946f8e4b5c403f102c4a32ebdf6b
Fedor Ivanov <ivnfedor@gmail.com>
2022-07-03 22:50:55 +0300

    Make the example more complex

--- example/bot/src/main.rs
+++ example/bot/src/main.rs
@@ -1,4 +1,4 @@
-use tbot::{bot, Bot};
+use tbot::{bot, prelude::*, Bot};
 
 fn build_bot() -> Bot {
     let mut builder = bot::Builder::with_env_token("BOT_TOKEN");
@@ -12,8 +12,21 @@ fn build_bot() -> Bot {
 
 #[tokio::main]
 async fn main() {
-    let bot = build_bot();
+    let mut bot = build_bot().event_loop();
 
-    let response = bot.get_me().call().await;
-    println!("{response:#?}");
+    bot.start(|context| async move {
+        context
+            .send_message("Hello! Send me a message and I'll reverse it.")
+            .call()
+            .await
+            .unwrap();
+    });
+
+    bot.text(|context| async move {
+        let reversed: String = context.text.value.chars().rev().collect();
+
+        context.send_message(reversed).call().await.unwrap();
+    });
+
+    bot.polling().start().await.unwrap();
 }
--- example/tests/Test.hs
+++ example/tests/Test.hs
@@ -7,20 +7,26 @@ import Data.Maybe (fromJust)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Network.Wai.Handler.Warp (Settings, defaultSettings, setPort)
-import System.Process (CreateProcess(env), createProcess, proc)
+import System.Process
+    (CreateProcess(env), ProcessHandle, createProcess, proc, terminateProcess)
 
-import Server (startServer, waitForAction)
+import Server (Server, modifyState, startServer, waitForAction)
 import Server.Actions (ActionKind(SendMessage))
 import qualified Server.Token as Token
 
+import Data.Time (getCurrentTime)
 import ServerState (ServerState)
 import qualified ServerState
 import qualified ServerState.BotPermissions as BotPermissions
 import ServerState.Id (Id(Id))
 import ServerState.InitialBot (InitialBot(InitialBot))
 import qualified ServerState.InitialBot as InitialBot
+import ServerState.Time (Time(Time))
 import ServerState.User (User(User))
 import qualified ServerState.User as User
+
+userId :: Id
+userId = Id 1
 
 botToken :: Text
 botToken = "2:random-characters"
@@ -32,7 +38,7 @@ initialState :: ServerState
 initialState = ServerState.initialize (user :| []) (bot :| [])
   where
     user = User
-        { User.userId = Id 1
+        { User.userId = userId
         , User.firstName = "User"
         , User.lastName = Nothing
         , User.username = Nothing
@@ -59,14 +65,33 @@ botEnv =
     , ("BOT_API_URI", "http://localhost:8081/")
     ]
 
-startBot :: IO ()
+startBot :: IO ProcessHandle
 startBot = do
-    _ <- createProcess ((proc botPath []) { env = Just botEnv })
-    return ()
+    (_, _, _, process) <- createProcess
+        ((proc botPath []) { env = Just botEnv })
+    return process
+
+killBot :: ProcessHandle -> IO ()
+killBot = terminateProcess
+
+sendMessage :: Text -> Server -> IO ()
+sendMessage text server = do
+    currentTime <- Time <$> getCurrentTime
+    modifyState server $ do
+        _ <- ServerState.sendMessage userId botId currentTime text
+        return ()
 
 main :: IO ()
 main = startServer initialState serverSettings $ \server -> do
-    startBot
+    bot <- startBot
+
+    sendMessage "/start" server
     waitForAction botId SendMessage server
-    putStrLn "The bot has called `getMe`. Yay!"
+    putStrLn "The bot has replied to /start. Good!"
+
+    sendMessage "Hello!" server
+    waitForAction botId SendMessage server
+    putStrLn "The bot has replied to a text message. Great!"
+
+    killBot bot
     return ()
